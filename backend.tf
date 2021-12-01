9358521d6e593a788741c161005d22a8a401966a
danny-molnar <danny.molnar@cloudreach.com>
2021-12-01 16:19:06 +0000

    UPDATE - key for s3 backend updated

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket = "talent-academy-lambda-exercise"
-    key    = ".tfstates"
+    key    = "talent-academy-834539731159-tfstates/sprint2/week3"
     dynamodb_table = "terraform-lock"
   }
 }
