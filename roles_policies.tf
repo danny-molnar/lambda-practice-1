e36c9a4e1494887b2cf7cd66dda4906f28943dec
danny-molnar <danny.molnar@cloudreach.com>
2021-12-02 09:51:33 +0000

    UPDATE - added role for lambda

--- roles_policies.tf
+++ roles_policies.tf
@@ -1,7 +1,29 @@
 # IAM roles and policies
 
 # Resource for IAM role
-# TODO
+resource "aws_iam_role" "lambda_role" {
+  name = "var.lambda_role_name"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Sid = "assumerolelambda"
+            Principal = {
+                Service = "lambda.amazonaws.com"
+            }
+        }
+    ]
+  })
+
+  tags = {
+    "Name" = "var.lambda_role_name"
+  }
+}
+
+
 
 # Resource for policy to allow the lambda to access CW logs
 # TODO
