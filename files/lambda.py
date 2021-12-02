bb8e211669404681d339a8f098bb602d513fdf35
danny-molnar <danny.molnar@cloudreach.com>
2021-12-02 15:07:53 +0000

    UPDATE - refactored handler function that now prints the desired output

--- files/lambda.py
+++ files/lambda.py
@@ -1,10 +1,10 @@
 import boto3
 import json
 
-def pet_handler(event, context):
+def pet_handler(event, context): #:)
     s3 = boto3.client('s3')
 
-    # Listing buckets
+    # Listing existing buckets
     response = s3.list_buckets()
     print("Existing buckets: ")
     for bucket in response['Buckets']:
@@ -12,12 +12,22 @@ def pet_handler(event, context):
 
     print("---------------")
 
-    # Reading data from object
-    content_object = s3.get_object(Bucket="talent-academy-lambda-exercise", Key="sample_data.json")
-    text = content_object["Body"].read().decode()
-    pets=json.loads(text)
-    print(pets['pets'][2]['favFoods'])
+    # Printing favorite food
+    
+    pets_bucket = event["S3Bucket"] # getting the bucket name from event
+    data_file = event["S3Prefix"] # getting data file name from event
+    pet_name = event["PetName"] # getting pet name from event
+    
+    result = s3.get_object(Bucket = pets_bucket, Key = data_file)
+    data_str = result["Body"].read().decode() # this is where i spent ages, because this makes data to be a string, not an object...
+    data = json.loads(data_str) # this now converts the previous string into a dictionary
+    
+    # printing the list of foods that matches the pet name
+    for index in data["pets"]:
+        if index["name"] == pet_name:
+            print(f"My name is {pet_name} and my favourite foods are as follows:")
+            for food in index["favFoods"]:
+                print(food)
+    
 
     print("---------------")
-
-    # Reading data from S3 based on source event
