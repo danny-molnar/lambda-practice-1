ed4dcf32c6ace445bc4ee40e26fe215403d2ba0c
danny-molnar <danny.molnar@cloudreach.com>
2021-12-02 12:14:41 +0000

    UPDATE - lambda policy and role/policy attachment

--- roles_policies.tf
+++ roles_policies.tf
@@ -23,11 +23,52 @@ resource "aws_iam_role" "lambda_role" {
   }
 }
 
-
-
 # Resource for policy to allow the lambda to access CW logs
-# TODO
+resource "aws_iam_policy" "lambda_cw_access_policy" {
+  name = "lambda_cw_access"
+  path = "/"
+  description = "This policy will allow the lambda function to access CloudWatch logs"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid" : "cwlogaccess"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*" # allowing all CloudWatch Log resources
+      },
+      {
+        "Sid" : "s3List",
+        "Action" : [
+          "s3:List*"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "s3access",
+        "Action" : [
+          "s3:GetObject",
+          "s3:PutObject",
+        ],
+        "Effect" : "Allow",
+        "Resource" : [
+          "arn:aws:s3:::${var.pets_bucket_name}", # allowing the bucket ITSELF to be accessed
+          "arn:aws:s3:::${var.pets_bucket_name}/*" # wildcard denoting everything in bucket will be accessed
+        ]
+      }
+    ]
+  })
+}
 
 # Attaching the policy to the role
-# TODO
+resource "aws_iam_role_policy_attachment" "lambda-role-policy-attachment" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_cw_access_policy.arn
+}
 
