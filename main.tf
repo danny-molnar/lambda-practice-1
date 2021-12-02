7f07c49d6d27a883d69511b19c8da62914b19514
danny-molnar <danny.molnar@cloudreach.com>
2021-12-02 12:25:28 +0000

    UPDATE - added data archive resource

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 # Main or rather misc.tf, where in this case the data archive will live
+# A bit of explanation:
+# this resource will create an archive file, whose type is zip, from the lambda.py
 
-#TODO create data archive
-
+data "archive_file" "pets_script" {
+    type = "zip"
+    source_file = "${path.module}/files/${var.script_filename}.py"
+    output_file = "${path.module}/files/${var.script_filename}.zip"
+}
