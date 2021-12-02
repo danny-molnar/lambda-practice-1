0a9a297b5734bf6618af75889fa106b1bbe7225e
danny-molnar <danny.molnar@cloudreach.com>
2021-12-02 12:44:55 +0000

    UPDATE - added lambda function resource

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,12 @@
 # This contains the lambda function resource
 
-# TODO: add lambda function definition (nb: here lambda-function will mean AWS Lambda function definition)
+resource "aws_lambda_function" "pets_lambda" {
+  filename = data.archive_file.pets_script.output_path
+  function_name = var.script_filename
+  role = aws_iam_role.lambda_role.arn
+  handler = "${var.script_filename}.pet_handler"
+
+  source_code_hash = data.archive_file.pets_script.output_base64sha256
+
+  runtime = "python3.8"
+}
