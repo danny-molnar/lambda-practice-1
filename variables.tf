bed15965e890b76bdb6654dc81071ebeeaa12987
danny-molnar <danny.molnar@cloudreach.com>
2021-12-02 09:50:31 +0000

    UPDATE - added lambda role name var def

--- variables.tf
+++ variables.tf
@@ -5,6 +5,11 @@ variable "aws_region" {
 }
 
 variable "pets_bucket_name" {
-    description = "This stores the name for the bucketwhere the pets' info is stored"
+    description = "This stores the name for the bucket with the pets' information"
     type = string
+}
+
+variable "lambda_role_name" {
+    description = "This variable stores the name for the lambda IAM role"
+    default = "lambda_petfood_assume_role"
 }
