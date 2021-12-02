a525851233c406f6fcb857c79d043ec954dbedf9
danny-molnar <danny.molnar@cloudreach.com>
2021-12-02 12:44:31 +0000

    UPDATE - added handler function

--- files/lambda.py
+++ files/lambda.py
@@ -1,25 +1,23 @@
 import boto3
 import json
 
-# TODO - create handler function and incorporate the below functionalities
+def pet_handler(event, context):
+    s3 = boto3.client('s3')
 
-s3 = boto3.client('s3')
+    # Listing buckets
+    response = s3.list_buckets()
+    print("Existing buckets: ")
+    for bucket in response['Buckets']:
+        print(f'  {bucket["Name"]}')
 
-# Listing buckets
-response = s3.list_buckets()
-print("Existing buckets: ")
-for bucket in response['Buckets']:
-    print(f'  {bucket["Name"]}')
+    print("---------------")
 
-print("---------------")
+    # Reading data from object
+    content_object = s3.get_object(Bucket="talent-academy-lambda-exercise", Key="sample_data.json")
+    text = content_object["Body"].read().decode()
+    pets=json.loads(text)
+    print(pets['pets'][2]['favFoods'])
 
-# Reading data from object
-content_object = s3.get_object(Bucket="talent-academy-lambda-exercise", Key="sample_data.json")
-text = content_object["Body"].read().decode()
-pets=json.loads(text)
-print(pets['pets'][2]['favFoods'])
+    print("---------------")
 
-print("---------------")
-
-# Reading data from S3 based on source event
-
+    # Reading data from S3 based on source event
